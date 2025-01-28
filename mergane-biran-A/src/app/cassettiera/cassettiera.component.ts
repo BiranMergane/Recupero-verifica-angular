254154cdb79dfb6a5fc060df687d31595adc99dc
Biran Mergane <b.mergane2007@gmail.com>
2025-01-28 11:34:27 +0000

    Secondo commit

--- mergane-biran-A/src/app/cassettiera/cassettiera.component.ts
+++ mergane-biran-A/src/app/cassettiera/cassettiera.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CasellaPostaComponent } from '../casella-posta/casella-posta.component';
 
 @Component({
   selector: 'app-cassettiera',
-  imports: [],
+  imports: [CasellaPostaComponent],
   templateUrl: './cassettiera.component.html',
   styleUrl: './cassettiera.component.css'
 })
-export class CassettieraComponent {
+export class CassettieraComponent implements OnInit{
+  nomi: string[] = ["pino", "mino", "rino", "gino"]
+  constructor(){
 
+  }
+ ngOnInit(): void {
+   
+ }
 }
